8473068a2761ca03ef28202b13d0a3a7f3ac3673
Stephen Kahuria <stephenkahuria@ymail.com>
2021-05-31 18:35:15 +0300

    OOP Example added objects

--- oop_example.py
+++ oop_example.py
@@ -7,14 +7,27 @@ class car():
     def getspeed(self):
         print("Maximum speed is: ", self.speed)
 
+    def setSpeed(self, speed):
+        self.speed = speed
+
 
 BMW = car(2018,155)
 FORD = car(2016, 140)
 
-car.getspeed(BMW)
-car.getspeed(FORD)
+class Sedan(car): # child class
+    def accelerate(self):
+        print('137')
+    def openTrunk(self):
+            print('trunk has been opened')
 
-# or
+class SUV(car): # child class
+    def accelerate(self):
+        print('127')
 
 BMW.getspeed()
+BMW.setSpeed(143)
+BMW.getspeed()
 FORD.getspeed()
+Honda= Sedan(2018,150)
+Honda.getspeed()
+Honda.openTrunk()
